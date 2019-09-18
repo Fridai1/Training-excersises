5788f43d6b1d1d63c556e7c059f147e6f598a492
Nikolai <nikolaiviby3@gmail.com>
2019-09-18 19:29:29 +0200

    solution

---
+++ CountVowels/CountVowels/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VowelCounter counter = new VowelCounter();
+            
+            Console.WriteLine("please enter a word with vowels");
+            string word = Console.ReadLine();
+            Console.WriteLine(counter.Count(word));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CountVowels/CountVowels/VowelCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CountVowels
+{
+    public class VowelCounter
+    {
+        public string Count(string word)
+        {
+            Dictionary<string, int> voweldDictionary = new Dictionary<string, int>()
+            {
+                {"a", 0}, {"e", 0}, {"i", 0}, {"o", 0}, {"u", 0},
+            };
+            int numberOfVowels = 0;
+            char[] charArray = word.ToCharArray();
+            foreach (var c in charArray)
+            {
+                if (voweldDictionary.ContainsKey(c.ToString()))
+                {
+                    voweldDictionary[c.ToString()]++;
+                    numberOfVowels++;
+                }
+            }
+
+            return
+                $"there were a total of {numberOfVowels} vowels, out of those there where a: {voweldDictionary["a"]} e: {voweldDictionary["e"]} i: {voweldDictionary["i"]} o: {voweldDictionary["o"]} u: {voweldDictionary["u"]}";
+        }
+    }
+}
